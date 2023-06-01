4bcb2548c12a5a2300a4fd8e694da0f1a2705ad0
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-01 10:56:28 +0200

    (Navbar) menu burger anim

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -26,6 +26,10 @@ const Navbar = () => {
 		setToggle(false);
 	};
 
+	const toggleMenu = () => {
+		setToggle(!isToggled);
+	};
+
 	return (
 		<>
 			<div className={styles.navbar}>
@@ -38,7 +42,7 @@ const Navbar = () => {
 							className={styles.imgLogoStriped}
 						/>
 					</div>
-					<a href='#home'>D</a>
+					{/* <a href='#home'>D</a> */}
 				</div>
 				{targetReached ? (
 					<div className={styles.nav}>
@@ -47,14 +51,17 @@ const Navbar = () => {
 							<li>{t('PROJECT')}</li>
 							<li>Contact</li>
 						</ul>
+						<div className={styles.language}>FR</div>
 					</div>
 				) : (
 					<button
-						className={styles.menuBurger}
+						// className={styles.menuBurger}
+						className={`${styles.menuBurger} ${isToggled ? styles.opened : ''}`}
 						// onclick="this.classList.toggle('opened');this.setAttribute('aria-expanded', this.classList.contains('opened'))"
+						onClick={toggleMenu}
 						aria-label='Main Menu'
 					>
-						<svg width='100' height='100' viewBox='0 0 100 100'>
+						<svg width='50' height='50' viewBox='0 0 100 100'>
 							<path
 								className={`${styles.line} ${styles.line1}`}
 								d='M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058'
@@ -71,7 +78,6 @@ const Navbar = () => {
 					</button>
 				)}
 			</div>
-			<div className={styles.languageToggle}></div>
 			{isToggled && (
 				<div className={styles.menuToggled}>
 					<div className={styles.navContainer}>
@@ -121,7 +127,7 @@ const Navbar = () => {
 								></div>
 							</li>
 						</ul>
-						<div className={styles.languageToggle}></div>
+						<div className={styles.languageToggle}>FR</div>
 					</div>
 				</div>
 			)}
