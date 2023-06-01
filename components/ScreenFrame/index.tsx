b208f270e48356089a5215250dbe6a3bc7cf45ab
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-01 16:43:18 +0200

    (ScreenFrame)  fix frame scroll

--- components/ScreenFrame/index.tsx
+++ components/ScreenFrame/index.tsx
@@ -5,9 +5,13 @@ import styles from './screenFrame.module.scss';
 const ScreenFrame = () => {
 	return (
 		<>
-			<div className={`${styles.top} ${styles.sideFrame}`} />
+			<div className={`${styles.top} ${styles.sideFrame}`}>
+				<div className={styles.borderBottom} />
+			</div>
 			<div className={`${styles.right} ${styles.sideFrame}`} />
-			<div className={`${styles.bottom} ${styles.sideFrame}`} />
+			<div className={`${styles.bottom} ${styles.sideFrame}`}>
+				<div className={styles.borderTop} />
+			</div>
 			<div className={`${styles.left} ${styles.sideFrame}`} />
 		</>
 	);
