7918cfe4e71935545dc319019ceaa0db7781447f
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-12 11:23:08 +0200

    (Contact) style

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -9,14 +9,18 @@ const Contact = () => {
 	const { t } = useTranslation();
 
 	return (
-		<section className={styles.contact} id='contact'>
+		<section
+			className={styles.contact}
+			id='contact'
+			// onSubmit={handleSubmit}
+		>
 			<div className={styles.title}>
 				<h3>Contact</h3>
 				<div className={styles.patternContainer}>
 					<Image src={'/img/patternShurikenS.webp'} alt='pattern' fill />
 				</div>
 			</div>
-			<div className={styles.inputs}>
+			<form className={styles.inputs} method='post'>
 				<a href='mailto:dubois.jeremy33@gmail.com'>dubois.jeremy33@gmail.com</a>
 				<label className={styles.input} htmlFor='name'>
 					<input type='text' name='name' placeholder={t('NAME')} required />
@@ -28,9 +32,18 @@ const Contact = () => {
 					className={`${styles.input} ${styles.message}`}
 					htmlFor='message'
 				>
-					<input type='text' name='message' placeholder='Message' required />
+					<textarea name='message' placeholder='Message' required />
 				</label>
-			</div>
+				<div className={styles.btnContainer}>
+					<button
+						className={styles.btn}
+						// onSubmit={() => setShowConfirmModal(true)}
+					>
+						{t('SEND')}
+						<span>▶</span>
+					</button>
+				</div>
+			</form>
 			<Lines />
 		</section>
 	);
