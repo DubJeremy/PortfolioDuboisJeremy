7f6f21dfb6430e55c73d75a7508baabbfd189ff6
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-14 16:24:05 +0200

    (ScreenFrame) fix desk view

--- components/ScreenFrame/index.tsx
+++ components/ScreenFrame/index.tsx
@@ -5,13 +5,11 @@ import styles from './screenFrame.module.scss';
 const ScreenFrame = () => {
 	return (
 		<>
-			<div className={`${styles.top} ${styles.sideFrame}`}>
-				<div className={styles.borderBottom} />
-			</div>
+			<div className={`${styles.top} ${styles.sideFrame}`} />
+
 			<div className={`${styles.right} ${styles.sideFrame}`} />
-			<div className={`${styles.bottom} ${styles.sideFrame}`}>
-				<div className={styles.borderTop} />
-			</div>
+			<div className={`${styles.bottom} ${styles.sideFrame}`} />
+
 			<div className={`${styles.left} ${styles.sideFrame}`} />
 		</>
 	);
