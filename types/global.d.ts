e33c8f3d1ddf1095a97e1dd520c91d73f01bcad3
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-19 16:11:07 +0200

    (Projects) add content

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,12 +1,16 @@
-// declare namespace ...Types {
-// 	interface ... {
-// 		id: number;
-//         ...: ...;
-//         ...: ...;
-// 	}
-// 	interface ... {
-// 		id: number;
-// 		...: ...;
-// 		...: ...;
-// 	}
-// }
+declare namespace ProjectTypes {
+	interface Content {
+		id: number;
+		footer: boolean;
+		title: string;
+		desc: string;
+		link?: string;
+		techs: string[];
+		screens: string[];
+	}
+	// interface ... {
+	// 	id: number;
+	// 	...: ...;
+	// 	...: ...;
+	// }
+}
