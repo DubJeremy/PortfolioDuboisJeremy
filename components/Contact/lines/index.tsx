62b12e3ba15055cd1d73f44dfc4e708a8248d6f0
DubJeremy <dubois.jeremy33@gmail.com>
2023-06-22 10:18:20 +0200

    (Contact) anim scroll to contact for start

--- components/Contact/lines/index.tsx
+++ components/Contact/lines/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import useMediaQuery from '@/tools/useMediaQuery';
 
@@ -6,8 +6,38 @@ import styles from './lines.module.scss';
 
 const Lines = () => {
 	const [targetReached] = useMediaQuery(`(min-width: 768px)`);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const contactElement = document.getElementById('contact');
+
+			if (contactElement) {
+				const contactPosition = contactElement.getBoundingClientRect().top;
+				const windowHeight = window.innerHeight;
+
+				if (contactPosition < windowHeight * 0.8) {
+					const linesElement = document.getElementById('lines');
+					if (linesElement) {
+						linesElement.classList.add(styles.linesAnimate);
+					}
+				} else {
+					const linesElement = document.getElementById('lines');
+					if (linesElement) {
+						linesElement.classList.remove(styles.linesAnimate);
+					}
+				}
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<div className={styles.lines}>
+		<div id='lines' className={styles.lines}>
 			<div className={styles.line} />
 			<div className={styles.line} />
 			<div className={styles.line} />
