c094b0a4e8622f627abf8653f898a2df2bddd62a
DubJeremy <dubois.jeremy33@gmail.com>
2023-07-19 18:09:27 +0200

    (Projects x Screens) wip new style desk

--- components/Projects/screens/index.tsx
+++ components/Projects/screens/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+
+import useMediaQuery from '@/tools/useMediaQuery';
 
 import styles from './screens.module.scss';
 
@@ -12,6 +14,25 @@ const Screens = ({
 	endAnimation: boolean;
 	mobileApp: boolean;
 }) => {
+	const [targetReached] = useMediaQuery(`(max-width: 992px)`);
+	const [isInLandscape, setIsInLandscape] = useState<boolean | null>(null);
+	const [hovered, setHovered] = useState<boolean>(false);
+
+	useEffect(() => {
+		heightFunction();
+		window.addEventListener('resize', heightFunction, false);
+	}, []);
+
+	const heightFunction = () => {
+		if (typeof window !== 'undefined') {
+			if (window.innerHeight < window.innerWidth) {
+				setIsInLandscape(true);
+			} else {
+				setIsInLandscape(false);
+			}
+		}
+	};
+
 	return (
 		<div className={styles.screens}>
 			<div
@@ -37,9 +58,11 @@ const Screens = ({
 				/>
 			</div>
 			<div
-				className={`${styles.screen} ${styles[paths[2]]} ${
-					endAnimation ? styles.endAnimT : ''
-				} ${mobileApp ? styles.mobile : styles.desk}`}
+				className={`${styles.screen} ${
+					(!targetReached && !isInLandscape) || hovered ? styles[paths[2]] : ''
+				} ${endAnimation ? styles.endAnimT : ''} ${
+					mobileApp ? styles.mobile : styles.desk
+				}`}
 			>
 				<Image
 					src={`/img/screen/${paths[2]}.webp`}
@@ -48,9 +71,11 @@ const Screens = ({
 				/>
 			</div>
 			<div
-				className={`${styles.screen} ${styles[paths[3]]} ${
-					endAnimation ? styles.endAnimFo : ''
-				} ${styles.mobile}`}
+				className={`${styles.screen} ${
+					(!targetReached && !isInLandscape) || hovered ? styles[paths[3]] : ''
+				} ${endAnimation ? styles.endAnimFo : ''} ${styles.mobile} ${
+					targetReached && isInLandscape && hovered ? styles[paths[3]] : ''
+				}`}
 			>
 				<Image
 					src={`/img/screen/${paths[3]}.webp`}
@@ -59,9 +84,11 @@ const Screens = ({
 				/>
 			</div>
 			<div
-				className={`${styles.screen} ${styles[paths[4]]} ${
-					endAnimation ? styles.endAnimFif : ''
-				} ${mobileApp ? styles.mobile : styles.desk}`}
+				className={`${styles.screen} ${
+					(!targetReached && !isInLandscape) || hovered ? styles[paths[5]] : ''
+				} ${endAnimation ? styles.endAnimFif : ''} ${
+					mobileApp ? styles.mobile : styles.desk
+				}`}
 			>
 				<Image
 					src={`/img/screen/${paths[4]}.webp`}
